2f957f86d5f49c4390da60114f01e0ced1c9e555
Sofia Escobedo <drapaiton@gmail.com>
2021-04-03 10:33:29 -0600

    wip structure

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
-from .configs import *
+from src.core.configs import *
+from src.apps.menu import Ui_MainWindow
+
+__all__ = [Ui_MainWindow]
---
+++ src/core/configs.py
@@ -0,0 +1,4 @@
+from os import environ
+
+# PC_MICROSERVICE_URL = environ["PC_MICROSERVICE_URL"]
+# PRODUCTION = environ["PRODUCTION"]
